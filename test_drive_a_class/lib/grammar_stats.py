b32d1b6d335345325de4825759761c8739ae503c
Perran Thomas <perran.thomas@icloud.com>
2023-09-22 15:41:50 +0100

    Completed GrammarStats challenge

--- test_drive_a_class/lib/grammar_stats.py
+++ test_drive_a_class/lib/grammar_stats.py
@@ -1,6 +1,19 @@
 class GrammarStats():
     def __init__(self):
-        pass
+        self._text_correctness_log = []
 
     def check(self, text):
-        return text[0].isupper() and (text[-1] in ".!?")
+        result = text[0].isupper() and (text[-1] in ".!?")
+        self._text_correctness_log.append(result)
+        return result
+
+    def percentage_good(self):
+        if len(self._text_correctness_log) == 0:
+            raise Exception("No texts checked so far.")
+        else:
+            return round(
+                100 * sum([
+                    1 if was_correct else 0
+                    for was_correct in self._text_correctness_log
+                ]) / len(self._text_correctness_log)
+            )
--- test_drive_a_class/tets/test_grammar_stats.py
+++ test_drive_a_class/tets/test_grammar_stats.py
@@ -1,4 +1,6 @@
+import pytest
 from lib.grammar_stats import *
+GrammarStats = GrammarStats  # helps with pylance linter
 
 def tests_can_create_grammar_stats_object():
     stats = GrammarStats()
@@ -19,3 +21,48 @@ def test_check_text_ending_with_non_punctuation_character_fails():
     result = stats.check("Hello, world")
     assert result == False
 
+def test_can_call_percentage_good_on_new_grammar_stats_instance():
+    stats = GrammarStats()
+    with pytest.raises(Exception) as e:
+        stats.percentage_good()
+    error_message = str(e.value)
+    assert error_message == "No texts checked so far."
+
+def test_one_incorrect_text_causes_percentage_good_to_return_0():
+    stats = GrammarStats()
+    stats.check("hello, world")
+    # __ = stats.check("hello, world")
+    assert stats.percentage_good() == 0
+
+def test_one_correct_text_causes_percentage_good_to_return_100():
+    stats = GrammarStats()
+    stats.check("Hello, world!")
+    assert stats.percentage_good() == 100
+
+def test_one_each_of_correct_and_incorrect_gives_50_percent_good():
+    stats = GrammarStats()
+    stats.check("This one is correct.")
+    stats.check("this one is incorrect")
+    assert stats.percentage_good() == 50
+
+def test_two_correct_and_one_incorrect_gives_67_percent_good():
+    stats = GrammarStats()
+    texts = [
+        "First correct text.",
+        "Second correct text.",
+        "first incorrect text",
+    ]
+    for text in texts:
+        stats.check(text)
+    assert stats.percentage_good() == 67
+
+def test_one_correct_and_two_incorrect_gives_33_percent_good():
+    stats = GrammarStats()
+    texts = [
+        "First correct text.",
+        "first incorrect text",
+        "second incorrect text",
+    ]
+    for text in texts:
+        stats.check(text)
+    assert stats.percentage_good() == 33
