df1891244c70aac093c029a142c17b8f7ab359e4
Perran Thomas <perran.thomas@icloud.com>
2023-09-26 17:25:20 +0100

    Refactored diary.py in test_drive_a_class_system

--- test_drive_a_class_system/lib/diary.py
+++ test_drive_a_class_system/lib/diary.py
@@ -25,25 +25,21 @@ class Diary:
         )
 
     def find_best_entry_for_reading_time(self, wpm, minutes):
-        # Parameters:
-        #   wpm:     an integer representing the number of words the user can
-        #            read per minute
-        #   minutes: an integer representing the number of minutes the user has
-        #            to read
-        # Returns:
-        #   An instance of DiaryEntry representing the entry that is closest to,
-        #   but not over, the length that the user could read in the minutes
-        #   they have available given their reading speed.
         if (not isinstance(wpm, int)) or wpm <= 0:
             raise Exception("`wpm` argument was not a positive integer.")
         if (not isinstance(minutes, int)) or minutes < 0:
             raise Exception("`minutes` argument was not a non-negative integer.")
-        best_entry = max(
-            (
-                entry for entry in self._entries
-                if entry.reading_time(wpm) <= minutes
-            ),
-            default = None,
-            key = lambda entry: entry.reading_time(wpm)
+        entry_with_reading_time_tuples = (
+            (entry, entry.reading_time(wpm))
+            for entry in self._entries
+        )
+        entry_tuples_satisfying_time_limit = (
+            entry_tuple for entry_tuple in entry_with_reading_time_tuples
+            if entry_tuple[1] <= minutes
+        )
+        best_entry, __ = max(
+            entry_tuples_satisfying_time_limit,
+            default = (None, None),
+            key = lambda entry_tuple: entry_tuple[1]
         )
         return best_entry
